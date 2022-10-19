d6da5c5b6ac566e2ceafeea4f4455451f7a9f852
Uzoma S. Uzoma <usumg1165@gmail.com>
2022-10-19 21:04:24 +0100

    Add more functionality to my 'new-task' components

--- src/index.js
+++ src/index.js
@@ -49,21 +49,122 @@ function showForm(e) {
 
     addBtn.addEventListener('click', addTask);
     cancelBtn.addEventListener('click', hideForm);
+
+    // Add task with 'Enter' key
+    document.addEventListener('keydown', altAddTask);
 }
 
 function hideForm() {
     document.querySelector('.inbox p').classList.remove('hidden');
     document.querySelector('.form').remove();
+
+    // Remove this event whenever the form is not displayed
+    document.removeEventListener('keydown', altAddTask);
 }
 
 function addTask() {
-    const taskName = document.querySelector('.form input').value;
+    const taskNameVal = document.querySelector('.form input').value;
+
+    if (!taskNameVal) {
+        alert('Your task name cannot be empty');
+        return;
+    }
+
+    // Hide the form input
     hideForm();
 
+    // Add new task
     document.querySelector('.inbox p').insertAdjacentHTML('beforebegin', newTask);  
 
     // Set the task name to equal the value typed in the form input
-    document.querySelector('.task-name').innerHTML = taskName;
+    const taskName = document.querySelector('.task-name');
+    taskName.innerHTML = taskNameVal;
+
+    // Change task name
+    const nameInput = document.querySelector('.name-inp');
+    
+    taskName.addEventListener('click', showTaskNameInput.bind(taskName, nameInput));
+    nameInput.addEventListener('change', setNewTaskName.bind(nameInput, taskName));
+
+    // Set the date 
+    const dateVal = document.querySelector('.date-val');
+    const dateInput = document.querySelector('#task-date');
+
+    dateInput.addEventListener('change', setDate.bind(dateInput, dateVal));
+    dateVal.addEventListener('click', showDatePicker.bind(dateVal, dateInput));
+
+    // Cancel the task when checkbox is clicked
+    const taskCheckbox = document.querySelector('.task-checkbox');
+    taskCheckbox.addEventListener('click', toggleTaskCancel);
+
+    // Remove task
+    document.querySelector('.remove-task').addEventListener('click', removeTask);
+
+}
+
+function altAddTask(e) {
+    if (e.key == 'Enter') addTask();
+    return;
+}
+
+function showTaskNameInput(elem) {
+    // Hide the task name and display name input
+    elem.classList.remove('hidden');
+    this.classList.add('hidden');
+}
+
+function setNewTaskName(elem) {
+    const newTaskName = this.value;
+
+    // Hide the name input and display the task name
+    elem.classList.remove('hidden');
+    this.classList.add('hidden');
+
+    elem.innerHTML = newTaskName;
+}
+
+function setDate(elem) {
+    const date = this.value;
+
+    // Hide the input and display its value
+    elem.classList.remove('hidden');
+    this.classList.add('hidden');
+
+    elem.innerHTML = date;
+}
+
+function showDatePicker(elem) {
+    // Hide the date and display date picker
+    elem.classList.remove('hidden');
+    this.classList.add('hidden');
+
+    // Reset the date picker
+    elem.value = '';
+}
+
+function toggleTaskCancel() {
+    if (taskCancelCounter % 2) {
+        // Hide the span.task-checkbox--full
+        document.querySelector('.task-checkbox--full').classList.add('hidden');
+
+        // Ungray the new-task div and display the date input if date-val is hidden
+        document.querySelector('.new-task').classList.remove('new-task--cancel');
+        if ([...document.querySelector('.date-val').classList].includes('hidden')) document.querySelector('#task-date').classList.remove('hidden');
+    }
+    else {
+        // Display the span.task-checkbox--full
+        document.querySelector('.task-checkbox--full').classList.remove('hidden');
+    
+        // Gray out the new-task div and hide the date input
+        document.querySelector('.new-task').classList.add('new-task--cancel');
+        document.querySelector('#task-date').classList.add('hidden');
+    }
+
+    taskCancelCounter++;
+}
+
+function removeTask() {
+    document.querySelector('.new-task').remove();
 }
 
 
@@ -72,6 +173,9 @@ function addTask() {
 let form;
 const display = init();
 const tabs = document.querySelectorAll('.nav-lis li');
+
+// Var
+let taskCancelCounter = 0;
 
 
 // Event Listeners
--- src/new-task.js
+++ src/new-task.js
@@ -1,11 +1,11 @@
 const html = `
 <div class="new-task">
     <div class="lt-side">
-        <span class="task-checkbox"></span>
-        <span class="task-name"></span>
+        <span class="task-checkbox"><span class="task-checkbox--full hidden"></span></span>
+        <span class="name"><input type="text" class="name-inp hidden"><span class="task-name"></span></span>
     </div>
     <div class="rt-side">
-        <span class="date"><span class="date-val"></span><input type="date" name="task-date" id="task-date"></span>
+        <span class="date"><span class="date-val hidden"></span><input type="date" name="task-date" id="task-date"></span>
         <span class="remove-task"><i class="fa-solid fa-xmark"></i></span>
     </div>
 </div>
